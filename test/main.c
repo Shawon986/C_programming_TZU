ba286eed404abb8d27163264ad98e9dc26e5eb31
Shawon <shawonh986@gmail.com>
2023-10-14 04:09:40 -0700

    upper to lowercase function

--- test/main.c
+++ test/main.c
@@ -2,31 +2,9 @@
 
 int main()
 {
-    printf("Hello world!\n");
 
-    // Function to print value taken from user
+//Q 1
 /*
-    int num;
-    printf("Enter an integer number");
-    scanf("%d",&num);
-    printf("you have pressed %d\n",num);
-
-    float num1;
-    printf("Enter a float number :");
-    scanf("%f",&num1);
-    printf("you have pressed %f\n",num1);
-
-    double num3;
-    printf("Enter the value for double :");
-    scanf("%lf",&num3);
-    printf("You have pressed %lf\n",num3);
-
-    char name;
-    printf("Enter your name");
-    scanf("%s",&name);
-    printf("You have pressed :%c",name);
-    */
-
     int first,second,third,sum,multiply,average;
      printf("Enter three integers: ");
     scanf("%d %d %d", &first, &second, &third);
@@ -39,34 +17,15 @@ int main()
 
     printf("%d + %d +%d = %d is the sum number \n", first, second,third, sum);
     printf("%d * %d *%d = %d is the product number\n", first,second,third, multiply);
-    printf("(%d + %d + %d)/3=%d is the average number",first,second,third,average);
+    printf("(%d + %d + %d)/3=%d is the average number \n",first,second,third,average);
+*/
 
+//Q 2
 
-
-
-
-
-    /*
-    int i ;
-    float f;
-    double d;
-    char c;
-
-    printf("size of i : %d bytes \n",sizeof(i));
-    printf("size of f : %d bytes \n",sizeof(f));
-    printf("size of d : %d bytes \n",sizeof(d));
-    printf("size of c : %d bytes \n",sizeof(c));
-
-    */
-
-    //ASCII to Character
-    /*
-    int n;
-    printf("Enter an ASCII value :");
-    scanf("%d",&n);
-    printf("Here is the character value :%c",n);
-
-    */
+    char lower;
+    printf("Enter any Uppercase charecter \n");
+    scanf("%c",&lower);
+    printf("Here is the Lowercase character %c:",lower+32);
 
 
 
